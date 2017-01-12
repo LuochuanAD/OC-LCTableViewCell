c746f7f96b7a8215b3edff467dba4238bf41255b
care <care@caredeimac-3.local>
2017-01-12 12:15:07 +0800

    Initial Commit

---
+++ LCTableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCTableViewCell
+//
+//  Created by care on 17/1/12.
+//  Copyright © 2017年 luochuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCTableViewCell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LCTableViewCell
+//
+//  Created by care on 17/1/12.
+//  Copyright © 2017年 luochuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
